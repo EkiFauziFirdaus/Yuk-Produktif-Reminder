e752b31f841b162414d626503f4b140c159707b6
exagradia <exagradia@outlook.com>
2017-05-25 14:47:26 +0700

    Configure Client model

--- src/main/java/com/yukproduktif/reminder/model/Client.java
+++ src/main/java/com/yukproduktif/reminder/model/Client.java
@@ -1,5 +1,53 @@
 package com.yukproduktif.reminder.model;
 
-public class Client {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "tb_client")
+public class Client {
+	
+	private int id;
+	private String callback;
+	private String accessToken;
+	
+	@Id
+	@Column(name = "client_id", unique = true, nullable = false)
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Column(name = "client_callback", nullable = false)
+	public String getCallback() {
+		return callback;
+	}
+	
+	public void setCallback(String callback) {
+		this.callback = callback;
+	}
+	
+	@Column(name = "client_access_token", nullable = false)
+	public String getAccessToken() {
+		return accessToken;
+	}
+	
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+	
+	public Client() {}
+
+	public Client(int id, String callback, String accessToken) {
+		super();
+		this.id = id;
+		this.callback = callback;
+		this.accessToken = accessToken;
+	}
+	
 }
