93a49a15043f99b33da9450b12a8b0bc0e032d54
exagradia <exagradia@outlook.com>
2017-05-25 14:31:05 +0700

    Initial Commit

---
+++ src/main/java/com/yukproduktif/reminder/YukProduktifReminderApplication.java
@@ -0,0 +1,12 @@
+package com.yukproduktif.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YukProduktifReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YukProduktifReminderApplication.class, args);
+	}
+}
