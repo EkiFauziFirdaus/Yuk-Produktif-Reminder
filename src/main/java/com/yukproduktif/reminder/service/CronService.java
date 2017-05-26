0157c25061c7dce52c269eb2be393eb57e3c0efb
exagradia <exagradia@outlook.com>
2017-05-26 07:26:03 +0700

    Configure Cron service

--- src/main/java/com/yukproduktif/reminder/service/CronService.java
+++ src/main/java/com/yukproduktif/reminder/service/CronService.java
@@ -1,5 +1,20 @@
 package com.yukproduktif.reminder.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.yukproduktif.reminder.repository.ClientRepository;
+
 public class CronService {
+	
+	@Autowired
+	ClientRepository clientRepo;
+	
+	@Scheduled(cron = "*/1 * * * * *")
+	public void cronToClient() {
+		for (int i = 0; i < clientRepo.count(); i++) {
+			System.out.println(clientRepo.findOne(i).getAccessToken());
+		}
+	}
 
 }
