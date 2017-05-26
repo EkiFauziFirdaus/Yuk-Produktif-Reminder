274f84d7292c18849ff9981fc74534b9e7497670
exagradia <exagradia@outlook.com>
2017-05-26 09:11:18 +0700

    Configure Prayer model

--- src/main/java/com/yukproduktif/reminder/model/Prayer.java
+++ src/main/java/com/yukproduktif/reminder/model/Prayer.java
@@ -1,5 +1,55 @@
 package com.yukproduktif.reminder.model;
 
+import java.sql.Time;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_prayer")
 public class Prayer {
+	
+	private int id;
+	private String name;
+	private Time time;
+	
+	@Id
+	@Column(name = "prayer_id", unique = true, nullable =  false)
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Column(name = "prayer_name", unique = true, nullable = false)
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name = "prayer_time", nullable = false)
+	public Time getTime() {
+		return time;
+	}
+	
+	public void setTime(Time time) {
+		this.time = time;
+	}
+	
+	public Prayer() {}
+
+	public Prayer(int id, String name, Time time) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.time = time;
+	}
 	
 }
