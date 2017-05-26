5dd0c6f0efeaa9d922379b03264ee021497a379c
exagradia <exagradia@outlook.com>
2017-05-26 08:03:37 +0700

    Configure Cron controller

--- src/main/java/com/yukproduktif/reminder/controller/CronController.java
+++ src/main/java/com/yukproduktif/reminder/controller/CronController.java
@@ -1,5 +1,25 @@
 package com.yukproduktif.reminder.controller;
 
-public class CronController {
+import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+import com.yukproduktif.reminder.repository.ClientRepository;
+
+@Controller
+public class CronController {
+	
+protected Logger logger = Logger.getLogger(CronController.class.getName());
+	
+	@Autowired
+	ClientRepository clientRepo;
+	
+	@Scheduled(cron = "*/1 * * * * *")
+	public void cronToClient() {
+		for (int i = 1; i <= clientRepo.count(); i++) {
+			logger.info("Token: "+clientRepo.findOne(i).getAccessToken());
+		}
+	}
 }
