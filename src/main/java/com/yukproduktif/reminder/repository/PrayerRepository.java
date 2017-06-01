8cb9501dac37466e7983c4320be19937a94a265d
exagradia <exagradia@outlook.com>
2017-06-01 10:31:47 +0700

    Add function to find by status field

--- src/main/java/com/yukproduktif/reminder/repository/PrayerRepository.java
+++ src/main/java/com/yukproduktif/reminder/repository/PrayerRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.yukproduktif.reminder.model.Prayer;
 
 public interface PrayerRepository extends CrudRepository<Prayer, Integer> {
+	
+	Prayer findByStatus(Boolean status);
 
 }
