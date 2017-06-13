3703bc00cd53b5566a90e6b7f2ac8e9b7a263125
exagradia <exagradia@outlook.com>
2017-06-13 20:58:41 +0700

    Configure jar deployment to azure.

--- src/main/java/com/yukproduktif/reminder/YukProduktifReminderApplication.java
+++ src/main/java/com/yukproduktif/reminder/YukProduktifReminderApplication.java
@@ -2,18 +2,11 @@ package com.yukproduktif.reminder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class YukProduktifReminderApplication extends SpringBootServletInitializer {
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(YukProduktifReminderApplication.class);
-	}
+public class YukProduktifReminderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YukProduktifReminderApplication.class, args);
